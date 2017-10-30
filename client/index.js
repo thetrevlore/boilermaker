76f621faa03d5383688f4ebc0a18c2974f75de93
Gabriel Defazio <gabrieldefazio@Gabriels-MacBook-Air.local>
2017-10-30 17:00:26 -0400

    webpack configged

---
+++ client/index.js
@@ -0,0 +1,5 @@
+'use strict'
+import React from 'react';
+import { render } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const port = 1313
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,12 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', require('./api'));
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
-
-
-
-
-app.use((err, req, res, next) => {
-  res.status(404).json(err.message);
+app.listen(port, ()=>{
+  console.log('server listening')
 });
